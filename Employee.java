cf98b115affc7df85afc1bcb1a062f9bf2a77b4d
vijaymaske90 <113819449+vijaymaske90@users.noreply.github.com>
2022-09-19 14:56:12 +0530

    Add files via upload
    Project of OOPS

---
+++ Employee.java
@@ -0,0 +1,16 @@
+
+public class Employee {
+private String firstName;
+private String lastName;
+
+public Employee(String firstName, String lastName) {
+	this.firstName= firstName;
+	this.lastName= lastName;
+}
+public String getFirstName() {
+	return firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+}
